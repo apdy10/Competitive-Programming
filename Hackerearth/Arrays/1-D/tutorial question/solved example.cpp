d202d6985c93b503182f17cee895bd819f6434b3
Arindam Priyadarshi <51259879+apdy10@users.noreply.github.com>
2022-03-30 23:03:30 +0530

    Create solved example.cpp

---
+++ Hackerearth/Arrays/1-D/tutorial question/solved example.cpp
@@ -0,0 +1,11 @@
+//Processing an Array
+#include<iostream>
+using namespace std;
+int main(){
+  // Array declaration and initialization
+  int arr[5] = {4,12,7,5,9};
+  //Iterate over the Array
+  for(int i=0; i<5;i++){
+    cout<<arr[i]<<endl;
+  }
+}
